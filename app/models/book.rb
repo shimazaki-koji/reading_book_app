0b9da51525af962a19023ce65574986dbb316db8
島崎晃次 <shimazakikoji@shimazakikojiMacBook-Air.local>
2021-05-22 12:01:27 +0900

    validatesの設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,14 @@ class Book < ApplicationRecord
   #has_many :comments
   #has_one_attached :image
   #belongs_to :user
+
+  #extend ActiveHash::Associations::ActiveRecordExtensions
+    #belongs_to_active_hash :contents
+
+  with_options presence: true do
+    validates :title
+    validates :learn
+    validates :contents
+  end
+
 end
