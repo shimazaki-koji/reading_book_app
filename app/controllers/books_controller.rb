59dbd203106d942c344460846213dfdbc7d9186b
島崎晃次 <shimazakikoji@shimazakikojiMacBook-Air.local>
2021-05-24 20:15:01 +0900

    マイページの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :learn, :contents)
+    params.require(:book).permit(:title, :learn, :contents).merge(user_id: current_user.id)
   end
 
   def set_book
